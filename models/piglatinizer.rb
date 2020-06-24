2f80abbb0ee1dd146d1940ffe44825b344faf206
DKendle <dina.kendle@gmail.com>
2020-06-24 11:37:47 -0400

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -5,6 +5,13 @@ class PigLatinizer
   
     end
 
-    def piglatinize(input) 
+    def piglatinize(input)
+      arr = input.split(//)
+      if arr.first # is a vowel
+        #add "way" to the end of the word
+      else 
+        ## take the first letter of the array
+        ## add it to the end and add "ay"
+      binding.pry
     end
 end
